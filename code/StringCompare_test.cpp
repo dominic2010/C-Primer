edb6590fe9c056a07ded4223bf99faaa3c2cfbae
xiaoyao <dominic2010@126.com>
2016-06-25 20:00:42 +0800

    1

--- code/StringCompare_test.cpp
+++ code/StringCompare_test.cpp
@@ -14,12 +14,14 @@ TEST(StringCompare,case1)
 {
 	StringCompare s1("huang");
 	std::string  s2 ="huang" ;
-	EXPECT_TRUE(s1.isEqual(s2));
+	EXPECT_TRUE(s1.compare(s2));
 }
 
 TEST(StringCompare,case2)
 {
 	StringCompare s1("huang");
 	std::string  s2 ="huang1" ;
-	EXPECT_FALSE(s1.isEqual(s2));
+	EXPECT_EQ(s2, s1.compare(s2));
 }
+
+
